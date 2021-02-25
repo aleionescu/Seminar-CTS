fac426fa139babca602533a06735ed75b855bc66
aleionescu <aleionescu99@gmail.com>
2021-02-25 19:06:55 +0200

    commitul trei

--- Seminar01/src/Pachet01/Program.java
+++ Seminar01/src/Pachet01/Program.java
@@ -1,6 +1,7 @@
 package Pachet01;
 import Clase.Girafa;
 import Clase.Zoo;
+import Clase.Zebra;
 
 public class Program {
 
@@ -10,9 +11,13 @@ public class Program {
 		Girafa girafa01 = new Girafa("girafa01");
 		Girafa girafa02 = new Girafa("girafa02");
 		
+		Zebra zebra01 = new Zebra("zebra01");
+		Zebra zebra02 = new Zebra("zebra02");
+		
 		zoo.addAnimal(girafa01);
 		zoo.addAnimal(girafa02);
-		
+		zoo.addAnimal(zebra01);
+		zoo.addAnimal(zebra02); 
 		zoo.feedAllAnimals();
 	}
 }
