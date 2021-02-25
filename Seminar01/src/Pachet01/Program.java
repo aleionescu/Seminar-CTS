97c51d9b6910ae088d0b8962faa843c1965c25fc
aleionescu <aleionescu99@gmail.com>
2021-02-25 18:57:41 +0200

    commit02

--- Seminar01/src/Pachet01/Program.java
+++ Seminar01/src/Pachet01/Program.java
@@ -1,8 +1,18 @@
 package Pachet01;
+import Clase.Girafa;
+import Clase.Zoo;
 
 public class Program {
 
 	public static void main(String [] args) {
+		Zoo zoo = new Zoo();
 		
+		Girafa girafa01 = new Girafa("girafa01");
+		Girafa girafa02 = new Girafa("girafa02");
+		
+		zoo.addAnimal(girafa01);
+		zoo.addAnimal(girafa02);
+		
+		zoo.feedAllAnimals();
 	}
 }
